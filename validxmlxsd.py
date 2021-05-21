5ea959ddbabcbd76c0787d7a751de154a334154f
masygreen <masygreen@gmail.com>
2021-05-21 12:38:33 +0300

    integrate colorama

--- validxmlxsd.py
+++ validxmlxsd.py
@@ -1,28 +1,21 @@
+import codecs
 import datetime
 import os
-import codecs
 
 import keyboard
+import sys
+from colorama import Fore, Back, Style, init, AnsiToWin32
 from lxml import etree
-
-class bcolors:
-    HEADER = '\033[95m'
-    OKBLUE = '\033[94m'
-    OKGREEN = '\033[92m'
-    WARNING = '\033[93m'
-    FAIL = '\033[91m'
-    ENDC = '\033[0m'
-    BOLD = '\033[1m'
-    UNDERLINE = '\033[4m'
 
 
 def writelog(logfile, message):
     with codecs.open(logfile, 'a', encoding='utf-8') as f:
         f.write(f"{message}\n")
 
+
 def validate(xsdfile, xmlfile, logfile):
     msg = f'Process: {datetime.datetime.now()}\n{xmlfile=}\n{xsdfile=}\n'
-    print(f'{bcolors.OKBLUE}{msg}')
+    print(f'{Fore.LIGHTBLUE_EX}{msg}', file=stream)
     writelog(logfile, msg)
 
     xmlschema_doc = etree.parse(xsdfile)
@@ -30,21 +23,21 @@ def validate(xsdfile, xmlfile, logfile):
     doc = etree.parse(xmlfile)
     if xmlschema.validate(doc):
         msg = 'OK'
-        print(f'{bcolors.OKGREEN}{msg}')
+        print(f'{Fore.GREEN}{msg}', file=stream)
         writelog(logfile, msg)
     else:
-        log = str(xmlschema.error_log)
         msg = f'Error:'
-        print(f"{bcolors.FAIL}{msg}")
+        print(f"{Fore.RED}{msg}", file=stream)
         writelog(logfile, msg)
         for error in xmlschema.error_log:
             msg = "ERROR ON LINE %s: %s" % (error.line, error.message.encode("utf-8"))
-            print(f"{bcolors.FAIL}{msg}")
+            print(f'{Fore.RED}{msg}', file=stream)
             writelog(logfile, msg)
         writelog(logfile, '______________________________________________\n\n')
 
 
 def main():
+    print(Style.RESET_ALL, file=stream)
     for in_file in os.listdir(os.getcwd()):
         if os.path.isfile(in_file) and in_file.endswith(".xml"):
             in_filename = in_file.split('.')[0]
@@ -52,16 +45,20 @@ def main():
             schema_file = f'{cur_dir}\\{in_filename}.xsd'
             log_file = f'{cur_dir}\\{in_filename}.log'
             if os.path.exists(schema_file):
-                print(f"{bcolors.HEADER}{bcolors.BOLD}File: {in_file}")
+                print(f"{Fore.YELLOW}==Start: '{in_file}' ==", file=stream)
                 validate(schema_file, xml_file, log_file)
-                print(f"{bcolors.OKBLUE}______________________________\n")
+                print(f"{Fore.YELLOW}==End: '{in_file}' ====", file=stream)
+                print(Style.RESET_ALL, file=stream)
 
 
 if __name__ == "__main__":
-    print(f"{bcolors.HEADER}{bcolors.BOLD}Last update: Cherepanov Maxim masygreen@gmail.com (c), 05.2021")
-    print(f"{bcolors.HEADER}{bcolors.BOLD}Find file the same name in current folder (*.xml; *.xsd)")
-    print(f"{bcolors.OKBLUE}______________________________")
+    init(wrap=False)
+    stream = AnsiToWin32(sys.stderr).stream
+
+    print(f"{Fore.CYAN}Last update: Cherepanov Maxim masygreen@gmail.com (c), 05.2021", file=stream)
+    print(f"{Fore.CYAN}Find file the same name in current folder (*.xml; *.xsd)", file=stream)
+    print(f"{Fore.CYAN}======================PROCESS==================", file=stream)
     cur_dir = os.getcwd()
     main()
-    print(f'{bcolors.HEADER}\n\n*All Process done\n*Press Space to Exit... It the longest shortcut \_(o0)_\...')
+    print(f'{Fore.CYAN}*All Process done\n*Press Space to Exit... It the longest shortcut \_(o0)_\...', file=stream)
     keyboard.wait("space")
